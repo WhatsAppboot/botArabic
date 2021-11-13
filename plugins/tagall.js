e85cc41063cad5f62f86d54b43f727a682feaf60
WhatsAppboot <93272048+WhatsAppboot@users.noreply.github.com>
2021-11-13 13:57:56 +0300

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -113,7 +113,7 @@ else if (Config.WORKTYPE == 'public') {
 }));
 var stag_dsc = ''
 if (Config.LANG !== 'ML') stag_dsc = 'يرسل الرسالة إلى جميع الأعضاء في المجموعة🤖.'
-if (Config.LANG == 'ML') stag_dsc = 'يرسل رسالة رد إلى جميع أعضاء المجموعة. '🗣️.'
+if (Config.LANG == 'ML') stag_dsc = 'يرسل رسالة رد إلى جميع أعضاء المجموعة.'
 
 Asena.addCommand({pattern: 'bc$', fromMe: true, desc: stag_dsc }, (async (message, match) => {
     if (!message.reply_message) return await message.client.sendMessage(message.jid,SLang.NEED_REPLY, MessageType.text)
