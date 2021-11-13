594f8bab0a5a7aa7a910709f794d7a568a98425e
WhatsAppboot <93272048+WhatsAppboot@users.noreply.github.com>
2021-11-13 22:50:42 +0300

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -11,7 +11,7 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Amalser [OWNER]\n' // full name
             + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'TEL;type=CELL;type=VOICE;waid=967774217099:+967 774 217 099\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "MeZo [OWNER]", vcard: vcard}, MessageType.contact)
 }))
